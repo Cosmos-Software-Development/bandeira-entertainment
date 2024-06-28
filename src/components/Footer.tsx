acfaa70363ff35540ee16a109b6099c190fcafae
codebyallan <allan.pereira.dev@proton.me>
2024-06-28 11:05:32 -0300

    update footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,123 +1,122 @@
-import styled from "styled-components"
-import instaIcon from '../assets/instagram-icon.png'
-import whatsappIcon from '../assets/whatsapp-icon-small.png'
-import linkedinIcon from '../assets/linkedin-icon.png'
-import logo from '../assets/bandeira-logo.png'
+import styled from "styled-components";
+import instaIcon from "../assets/instagram-icon.png";
+import whatsappIcon from "../assets/whatsapp-icon-small.png";
+import linkedinIcon from "../assets/linkedin-icon.png";
+import logo from "../assets/bandeira-logo.png";
 
-export default function Footer(){
-    return(
-        <ContainerFooter>
-            <Container>
-            <ContainerSocial>
-                <p>Acompanhe em</p>
-                <ContainerIcons>
-                    <img src={instaIcon} />
-                    <img src={whatsappIcon} />
-                    <img src={linkedinIcon} />
-                </ContainerIcons>
-            </ContainerSocial>
-            <ContainerLinks>
-            <a href="#programa" >O PROGRAMA</a>
-                <a href="#consultores" >OS CONSULTORES</a>
-                <a href="#modulos"  >MÓDULOS DE IMERSÃO</a>
-                <a href="#investimento" >INVESTIMENTO</a>
-                <a href="#faq" >FAQ</a>
-                <a href="#contato" >FALE CONOSCO</a>
-            </ContainerLinks>
-            <ContainerLogo>
-                <img src={logo} />
-            </ContainerLogo>
-            </Container>
-            <ContainerCopy>
-                <p>Copyright © 2024. Bandeira Entretenimento</p>
-                <p>Todos os direitos reservados. CNPJ: 30.798.660/0001-20</p>
-            </ContainerCopy>
-        </ContainerFooter>
-    )
+export default function Footer() {
+  return (
+    <ContainerFooter>
+      <Container>
+        <ContainerSocial>
+          <p>Acompanhe em</p>
+          <ContainerIcons>
+            <img src={instaIcon} />
+            <img src={whatsappIcon} />
+            <img src={linkedinIcon} />
+          </ContainerIcons>
+        </ContainerSocial>
+        <ContainerLinks>
+          <a href="#home">O PROGRAMA</a>
+          <a href="#brands">AS MARCAS</a>
+          <a href="#method">NOSSO MÉTODO</a>
+          <a href="#objective">OBJETIVO</a>
+          <a href="#investment">AGENDAMENTO</a>
+          <a href="#faq">FAQ</a>
+        </ContainerLinks>
+        <ContainerLogo>
+          <img src={logo} />
+        </ContainerLogo>
+      </Container>
+      <ContainerCopy>
+        <p>Copyright © 2024. Bandeira Entretenimento</p>
+        <p>Todos os direitos reservados. CNPJ: 30.798.660/0001-20</p>
+      </ContainerCopy>
+    </ContainerFooter>
+  );
 }
 
-const ContainerFooter =styled.div`
-    padding-top: 54px;
-    padding-left: 25px;
-    padding-right: 20px;
-    padding-bottom: 32px;
-    background: ${props => props.theme.colors['gradient-1']};
-`
+const ContainerFooter = styled.div`
+  padding-top: 54px;
+  padding-left: 25px;
+  padding-right: 20px;
+  padding-bottom: 32px;
+  background: ${(props) => props.theme.colors["gradient-7"]};
+`;
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-    @media (min-width: 1024px){
-        flex-direction: row-reverse;
-        justify-content: space-around;
-        align-items: center;
-    }
-`
+  @media (min-width: 1024px) {
+    flex-direction: row-reverse;
+    justify-content: space-around;
+    align-items: center;
+  }
+`;
 
 const ContainerSocial = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 42px;
-    p {
-        font-family: "Montserrat", sans-serif;
-        font-weight: 700;
-        letter-spacing: -3%;
-        color: ${props => props.theme.colors['white']};
-        font-size: 14px;
-    }
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 42px;
+  p {
+    font-family: "Montserrat", sans-serif;
+    font-weight: 700;
+    letter-spacing: -3%;
+    color: ${(props) => props.theme.colors["white"]};
+    font-size: 14px;
+  }
+`;
 
 const ContainerIcons = styled.div`
-    display: flex;
-    gap: 16px;
-    justify-content: center;
-    align-items: center;
-    img {
-        width: 30px;
-    }
-`
+  display: flex;
+  gap: 16px;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 30px;
+  }
+`;
 
 const ContainerLinks = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 18px;
-    margin-top: 52px;
-    a {
-        font-family: "Montserrat", sans-serif;
-        text-decoration: none;
-        font-size: 14px;
-        font-weight: 400;
-        color: ${props => props.theme.colors['white']};
-    }
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 18px;
+  margin-top: 52px;
+  a {
+    font-family: "Montserrat", sans-serif;
+    text-decoration: none;
+    font-size: 14px;
+    font-weight: 400;
+    color: ${(props) => props.theme.colors["white"]};
+  }
+`;
 
 const ContainerLogo = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 42px;
-    img {
-        width: 132px;
-    }
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 42px;
+  img {
+    width: 132px;
+  }
+`;
 
 const ContainerCopy = styled.div`
-    margin-top: 42px;
-    p {
-        font-family: "Montserrat", sans-serif;
-        font-size: 10px;
-        color: ${props => props.theme.colors['white']};
-        letter-spacing: -3%;
-        font-weight: 300;
-        text-align: center;
-    }
+  margin-top: 42px;
+  p {
+    font-family: "Montserrat", sans-serif;
+    font-size: 10px;
+    color: ${(props) => props.theme.colors["white"]};
+    letter-spacing: -3%;
+    font-weight: 300;
+    text-align: center;
+  }
 
-    p:first-child {
-        font-weight: 700;
-    }
-
-`
+  p:first-child {
+    font-weight: 700;
+  }
+`;
--- src/theme/defaultTheme.ts
+++ src/theme/defaultTheme.ts
@@ -13,6 +13,7 @@ export const defaultTheme = {
         'gradient-4': 'linear-gradient(to bottom, #110f0f, #181616);',
         'gradient-5': 'linear-gradient(90deg, #9c9c9c 0%, #363636 100%);',
         'gradient-6': 'linear-gradient(179.47deg, #5e0ba3 9.92%, #ea2613 111.13%);',
+        'gradient-7': 'linear-gradient(179.92deg, #5E0BA3 0.07%, #EA2613 164.56%);',
         'gray-100':'#e6e6e6',
         'gray-400': '#434343',
         'gray-200': '#9a9a9a',
